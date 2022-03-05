d0413db82c4baeccbd380c50f119f243c71aa9fb
corupta <halitozsoy1584@gmail.com>
2022-03-05 20:36:12 +0300

    port to python3 and spidev, implement 4/7/10byte uid, some uid info

--- README.md
+++ README.md
@@ -1,21 +1,41 @@
 # MFRC522 RFID Card Reader
 
-#### MFRC522 Python Library for Orange Pi
+I ported the original library for my use-case:
+* I needed to be able to read 4/7/10 byte uids and nothing else
+* I did NOT need to implement any read/write for remaining sectors
+* I refer to [https://www.nxp.com/docs/en/application-note/AN10927.pdf](https://www.nxp.com/docs/en/application-note/AN10927.pdf) and ISO/IEC 14443 UIDs
+
+- [x] Modified the library to make it work on python3
+- [x] Replaced old spi with spidev
+- [x] Implemented support for reading 4/7/10byte uid
+- [x] Implement adding some metadata about uid
+- [ ] Test reading 4 byte UID
+- [ ] Test reading 7 byte UID
+- [ ] Test reading 10 byte UID (Cannot test, I don't have such a card)
+- [ ] Test reading 4 byte RID
+
+#### MFRC522 Python3 Library for Orange Pi
   * Preparation
   ```
-  $ sudo apt-get install python-dev
-  $ git clone https://github.com/lthiery/SPI-Py
-  $ cd SPI-Py
-  $ sudo python setup.py install
+  $ sudo apt-get install python3 python3-dev python3-pip
+  $ sudo pip3 install -r Requirements.txt
   ```
 
   * Using Example
   ```
-  $ git clone https://github.com/J-Rios/MFRC522-python-OPi
+  $ git clone https://github.com/corupta/MFRC522-python-OPi
   $ cd MFRC522-python-OPi
-  $ sudo python read.py
+  $ sudo python3 read.py --debug
   ```
 
 * [Mifare-RC522 and Orange PI](https://descubriendolaorangepi.wordpress.com/2017/04/09/gpio-en-python-conectando-un-lector-rfid-nfc-por-spi)
 
-* [Hardware SPI as a C Extension for Python](https://github.com/lthiery/SPI-Py)
+
+#### See Fork History
+
+Original repo is from 2014, and it has MIT license, the same license file is kept afterwards.
+
+* [@corupta/MFRC522-python-OPi](https://github.com/corupta/MFRC522-python-OPi)
+* [@J-Rios/MFRC522-python-OPi](https://github.com/J-Rios/MFRC522-python-OPi)
+* [@yongsukki/MFRC522-python](https://github.com/yongsukki/MFRC522-python)
+
--- read.py
+++ read.py
@@ -1,34 +1,90 @@
+#!/usr/bin/env python3
+
 import MFRC522
 import signal
+import time
+import json
+import sys
+
+debug_enabled = False
+if len(sys.argv) >= 2 and str(sys.argv[1]) == '--debug':
+  debug_enabled = True
 
 continue_reading = True
 MIFAREReader = MFRC522.MFRC522()
 
-cardA = [5,74,28,185,234]
-cardB = [83,164,247,164,164]
-cardC = [20,38,121,207,132]
-
 def end_read(signal, frame):
   global continue_reading
   continue_reading = False
-  print "Ctrl+C captured, ending read."
+  pass # print "Ctrl+C captured, ending read."
   MIFAREReader.GPIO_CLEEN()
 
 signal.signal(signal.SIGINT, end_read)
 
-while continue_reading:
-  (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
-  if status == MIFAREReader.MI_OK:
-    print "Card detected"
-  (status,backData) = MIFAREReader.MFRC522_Anticoll()
-  if status == MIFAREReader.MI_OK:
-    print "Card read UID: "+str(backData[0])+","+str(backData[1])+","+str(backData[2])+","+str(backData[3])+","+str(backData[4])
-    if  backData == cardA:
-      print "is Card A"
-    elif backData == cardB:
-      print "is Card B"
-    elif backData == cardC:
-      print "is Card C"
-    else:
-      print "wrong Card"
+def debug_print(*args, **kwargs):
+  global debug_enabled
+  if debug_enabled:
+    print(*args, **kwargs)
 
+def get_card_info(uid, iso_compliant=False):
+  manufacturer_code = None if len(uid) == 4 else uid[0]
+  res = {
+    'uidLength': len(uid),
+    'isoCompliant': iso_compliant,
+    'isRandomID': len(uid) == 4 and uid[0] == 0x08,
+    'isFixedNumberNonUniqueID': len(uid) == 4 and (uid[0] & 0x0F) == 0x0F,
+    'isRFU': len(uid) == 4 and uid[0] == 0xF8, # I do not know what an RFU is.
+    'manufacturerCode': manufacturer_code,
+  }
+  debug_print('Card Info', res)
+  return res
+
+def read_uid():
+  # read 4 byte (1 level), 7byte (2 levels), 10 byte (3 levels) cards
+  (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+  if status != MIFAREReader.MI_OK:
+    return
+  debug_print("Card detected")
+  uid = []
+  sek = 0
+  uid_complete = False
+  iso_compliant = False
+  for cascade_level in range(1,4):
+      (status,uid_part) = MIFAREReader.MFRC522_Anticoll(cascade_level=cascade_level)
+      if status != MIFAREReader.MI_OK:
+        return
+      debug_print("Cascade level {} read.".format(cascade_level), uid_part)
+      (status, sek) = MIFAREReader.MFRC522_SelectTag(uid_part, cascade_level=cascade_level)
+      if status != MIFAREReader.MI_OK:
+        return
+      debug_print("Cascade level {} select.".format(cascade_level), sek)
+      uid_complete = ((sek) & 0x04) == 0
+      if uid_complete:
+        uid.extend(uid_part[0:4])
+        break
+      else:
+        uid.extend(uid_part[1:4])
+      debug_print("UID not complete")
+  iso_compliant = ((sek) & 0x20) == 0x20
+  if not uid_complete:
+    debug_print("Failed to read UID successfully", uid)
+    return
+  debug_print("Successfully read UID", uid)
+  card_info = get_card_info(uid, iso_compliant)
+  return (uid, card_info)
+
+while continue_reading:
+  time.sleep(0.1)
+
+  res = read_uid()
+
+  if res is None:
+    continue
+
+  (uid, card_info) = res
+
+  json_res = "{}\n".format(json.dumps({ 'uid': uid, 'cardInfo': card_info }))
+
+  sys.stdout.write(json_res)
+  # stay idle for 1 second after reading a card
+  time.sleep(1)
